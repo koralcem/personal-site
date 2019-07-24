98edbf7a3acd08647aab830b172af59a01199044
Cem Schemel <koralcem@gmail.com>
2019-07-24 10:17:01 -0700

    Update React Burgle Bros cheat sheet to match web components version

--- js/BurgleBrosReact.js
+++ js/BurgleBrosReact.js
@@ -14,49 +14,66 @@ class RoomTable extends React.Component {
     this.state = {
       rooms: this.props.rooms
     }
+    this.handleClick = this.handleClick.bind(this)
   }
 
   handleClick(roomName) {
     let rooms = this.state.rooms.slice()
     rooms.find( room => room.name === roomName ).count -= 1
-    rooms = rooms.filter( room => room.count > 0 )
     this.setState({rooms: rooms})
   }
 
   render() {
-    const totalRoomCount = this.state.rooms.reduce((total, room) => {
-      return total + room.count
-    }, 0)
+    const numColumns = 2
+    let columnComponents = []
+    for (let columnIndex = 0; columnIndex < numColumns; columnIndex++) {
+      const stride = this.state.rooms.length / numColumns
+      const sliceStart = columnIndex * stride
+      const sliceEnd = (columnIndex + 1) * stride
+      columnComponents.push(<RoomColumn rooms={this.state.rooms.slice(sliceStart, sliceEnd)} onClick={this.handleClick}/>)
+    }
 
+    return <div className='card'>{columnComponents}</div>
+  }
+}
+
+class RoomColumn extends React.Component {
+  render () {
     return (
-      <table>
-        <tbody>
-          {this.state.rooms.map((room) =>
-            <RoomRow
-              key={room.name}
-              name={room.name}
-              remaining={room.count}
-              type={room.type}
-              onClick={() => this.handleClick(room.name) }
-            />
-          )}
-        </tbody>
-      </table>
+      <div className='column'>
+        {this.props.rooms.map((room) =>
+          <RoomRow
+            key={room.name}
+            name={room.name}
+            remaining={room.count}
+            type={room.type}
+            onClick={this.props.onClick}
+          />
+        )}
+      </div>
     )
   }
 }
 
 class RoomRow extends React.Component {
   render() {
+    let className = `room ${this.props.type}`
+    if (this.props.remaining === 0) {
+      className += ' out-of-game'
+    }
+
     return (
-      <tr className={this.props.type} onClick={() => this.props.onClick()}>
-        <td>{this.props.name}</td>
-        <td>{this.props.remaining}</td>
-      </tr>
+      <div className={className} onClick={() => {
+        if (this.props.remaining > 0) {
+          this.props.onClick(this.props.name)
+        }
+      }}>
+        <div>{this.props.remaining}</div>
+        <div>{this.props.name}</div>
+      </div>
     )
   }
 }
-
 
 const allRooms = [
   new RoomTile('Safe', 3, 'safe'),
